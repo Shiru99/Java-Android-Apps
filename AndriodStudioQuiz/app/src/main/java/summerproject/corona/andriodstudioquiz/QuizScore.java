86eb61b391778f707681fbd7ab31ecf0328f09aa
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-04 11:09:30 +0530

    Quiz Scorecard added

---
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizScore.java
@@ -0,0 +1,14 @@
+package summerproject.corona.andriodstudioquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizScore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_score);
+    }
+}

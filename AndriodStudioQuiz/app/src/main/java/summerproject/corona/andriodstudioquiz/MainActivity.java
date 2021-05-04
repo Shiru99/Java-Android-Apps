bfca821800bcb360a419f33527712044e4958220
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-04 09:29:15 +0530

    Log & Toast added for buttons

--- AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/MainActivity.java
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/MainActivity.java
@@ -3,12 +3,60 @@ package summerproject.corona.andriodstudioquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Start;
+    private Button NotAvailableJava;
+    private Button NotAvailablePython;
+    private Button NotAvailableDart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NotAvailableJava = findViewById(R.id.java_button);
+        NotAvailableJava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("QuizRequest","Request for Java quiz");
+                Toast.makeText(MainActivity.this, "Quiz not available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        NotAvailablePython = findViewById(R.id.python_button);
+        NotAvailablePython.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("QuizRequest","Request for Python quiz");
+                Toast.makeText(MainActivity.this, "Quiz not available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        NotAvailableDart = findViewById(R.id.dart_button);
+        NotAvailableDart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("QuizRequest","Request for Dart quiz");
+                Toast.makeText(MainActivity.this, "Quiz not available", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Start = findViewById(R.id.android_button);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("QuizRequest","Request for Android quiz");
+                Toast.makeText(MainActivity.this, "Quiz Started", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }

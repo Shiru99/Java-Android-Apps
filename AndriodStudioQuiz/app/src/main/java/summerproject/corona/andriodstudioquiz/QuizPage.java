db4dcd25236b6f3516a64f612fb15a80c252c977
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-04 11:06:38 +0530

    Quiz Page added

---
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
@@ -0,0 +1,13 @@
+package summerproject.corona.andriodstudioquiz;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_page);
+    }
+}

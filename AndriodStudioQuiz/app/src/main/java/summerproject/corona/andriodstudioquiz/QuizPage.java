17bb2661a949ce6c3af55099e846091beafcb4ce
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-04 14:37:13 +0530

    countdown timer added

--- AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
@@ -1,13 +1,18 @@
 package summerproject.corona.andriodstudioquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class QuizPage extends AppCompatActivity {
 
     private TextView title;
+    private TextView countdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,25 @@ public class QuizPage extends AppCompatActivity {
 
         title = findViewById(R.id.title);
         title.setText(subject);
+
+        countdown = (TextView) findViewById( R.id.countdown );
+        new CountDownTimer(600000, 1000)  // milli seconds
+        {
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            public void onTick(long millisUntilFinished) {
+                countdown.setText(
+                        String.format("%d min, %d sec",
+                                TimeUnit.MILLISECONDS.toMinutes( millisUntilFinished),
+                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)
+                                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+                        )
+                );
+            }
+
+            public void onFinish() {
+                countdown.setText("TimeOut");
+            }
+        }.start();
 
 
 

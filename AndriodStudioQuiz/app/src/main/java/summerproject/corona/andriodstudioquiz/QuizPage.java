fcda3c04d26fd9f72efeb903ced6c9b5ed91579a
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-05 12:54:16 +0530

    Completed

--- AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
@@ -26,8 +26,13 @@ public class QuizPage extends AppCompatActivity {
     private String[] Questions;
     private boolean[] Answers;
     private int score;
+    private int index=0;
     private String scoreText="0/0";
     public static  String EXTRA_QuizScore="summerproject.corona.codingquiz.key.quizscore"; // Unique
+
+//    public static void visitScorecard(){
+//        return;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,40 +41,82 @@ public class QuizPage extends AppCompatActivity {
 
         Log.d("AppLogs","Entered in Quiz Page");
 
+
         Intent intent = getIntent();
         String subject = intent.getStringExtra(MainActivity.EXTRA_QuizPage);
 
         title = findViewById(R.id.title);
         title.setText(subject);
 
-        if(title.toString().toLowerCase().equals("java")){
-            Questions = new String[]{"Q.1", "Q.2", "Q.3", "Q.4", "Q.5", "Q.6"};
-            Answers = new boolean[]{true,false,true,false,true,true};
+        // Questions for JAVA :
+        if(subject.toLowerCase().equals("java")){
+            Questions = new String[]{"In an instance method or a constructor, \"this\" is a reference to the current object.", "Garbage Collection is manual process.\n", "The JRE deletes objects when it determines that they are no longer being used. This process is called Garbage Collection", "Assignment operator is evaluated Left to Right", "All binary operators except for the assignment operators are evaluated from Left to Right", "Java programming is not statically-typed, means all variables should not first be declared before they can be used."};
+            Answers = new boolean[]{true,false,true,false,true,false};
             score = 0;
         }
 
 
-        buttonFalse = findViewById(R.id.buttonFalse);
+        final Intent intent_QuizScore = new Intent(this,QuizScore.class);
+
+        // ScoreCard Button
+        scorecard = (Button) findViewById(R.id.scorecard);
+        scorecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreText = score+"/"+Questions.length;
+                Log.d("AppLogs","Request for Score");
+                intent_QuizScore.putExtra(EXTRA_QuizScore,scoreText);
+                startActivity(intent_QuizScore);
+            }
+        });
+
+        question = findViewById(R.id.question);
+        question.setMovementMethod(new ScrollingMovementMethod()); // In case Question is long => need to scroll
+        question.setText(Questions[index]);
         buttonTrue = findViewById(R.id.buttonTrue);
+        buttonFalse = findViewById(R.id.buttonFalse);
+
 
         buttonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizPage.this, "Answer Recorded : True", Toast.LENGTH_SHORT).show();
+                if(Answers[index]){
+                    score++;
+
+                }
+                if(++index==Questions.length){
+                    Log.d("AppLogs","Quiz Over! redirect to scorecard");
+                    scorecard.performClick();
+                }else{
+                    question.setText(Questions[index]);
+                }
+
             }
         });
+
 
         buttonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizPage.this, "Answer Recorded : False", Toast.LENGTH_SHORT).show();
+                if(!Answers[index]){
+                    score++;
+
+                }
+                if(++index==Questions.length){
+                    Log.d("AppLogs","Quiz Over! redirect to scorecard");
+                    scorecard.performClick();
+                }else{
+                    question.setText(Questions[index]);
+                }
             }
         });
 
 
-
+        // CountDown
         countdown = (TextView) findViewById( R.id.countdown );
-        new CountDownTimer(600000, 1000)  // milli seconds
+        new CountDownTimer(30000*(Questions.length), 1000)  // milli seconds
         {
             @SuppressLint({"DefaultLocale", "SetTextI18n"})
             public void onTick(long millisUntilFinished) {
@@ -85,28 +132,14 @@ public class QuizPage extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             public void onFinish() {
                 countdown.setText("TimeOut");
+                scorecard.performClick();
             }
         }.start();
 
-        question = findViewById(R.id.question);
-        question.setMovementMethod(new ScrollingMovementMethod());
 
 
 
-        // ScoreCard Button
 
-        final Intent intent_QuizScore = new Intent(this,QuizScore.class);
-
-        scorecard = findViewById(R.id.scorecard);
-        scorecard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("AppLogs","Request for Score");
-                //  Toast.makeText(QuizPage.this, "Score", Toast.LENGTH_SHORT).show();
-                intent_QuizScore.putExtra(EXTRA_QuizScore,scoreText);
-                startActivity(intent_QuizScore);
-            }
-        });
 
 
 

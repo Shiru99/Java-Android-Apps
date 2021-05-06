4a0c50141099b9a04bffeceec3e779d9ca415aed
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-06 08:14:55 +0530

    code format improved

--- AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/MainActivity.java
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/MainActivity.java
@@ -15,23 +15,23 @@ public class MainActivity extends AppCompatActivity {
     private Button NotAvailableAndroid;
     private Button AvailablePython;
     private Button NotAvailableDart;
-    public static  String EXTRA_QuizPage="summerproject.corona.codingquiz.key.quizpage"; // Unique
+    public static String EXTRA_QuizPage = "summerproject.corona.codingquiz.key.quizpage"; // Unique
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("AppLogs","App Started");
+        Log.d("AppLogs", "App Started");
 
-        final Intent intent_QuizPage = new Intent(this,QuizPage.class);
+        final Intent intent_QuizPage = new Intent(this, QuizPage.class);
 
         AvailableJava = findViewById(R.id.java_button);
         AvailableJava.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("AppLogs","Request for Java quiz page");
+                Log.d("AppLogs", "Request for Java quiz page");
                 Toast.makeText(MainActivity.this, "All The Best", Toast.LENGTH_SHORT).show();
-                intent_QuizPage.putExtra(EXTRA_QuizPage,"Java");
+                intent_QuizPage.putExtra(EXTRA_QuizPage, "Java");
                 startActivity(intent_QuizPage);
             }
         });
@@ -40,9 +40,9 @@ public class MainActivity extends AppCompatActivity {
         AvailablePython.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("AppLogs","Request for Python quiz");
+                Log.d("AppLogs", "Request for Python quiz");
                 Toast.makeText(MainActivity.this, "All The Best", Toast.LENGTH_SHORT).show();
-                intent_QuizPage.putExtra(EXTRA_QuizPage,"Python");
+                intent_QuizPage.putExtra(EXTRA_QuizPage, "Python");
                 startActivity(intent_QuizPage);
             }
         });
@@ -51,28 +51,24 @@ public class MainActivity extends AppCompatActivity {
         NotAvailableAndroid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("AppLogs","Request for Android quiz");
+                Log.d("AppLogs", "Request for Android quiz");
                 Toast.makeText(MainActivity.this, "Quiz not available", Toast.LENGTH_SHORT).show();
                 // intent_QuizPage.putExtra(EXTRA_QuizPage,"Android");
                 // startActivity(intent_QuizPage);
             }
         });
 
-
         NotAvailableDart = findViewById(R.id.dart_button);
         NotAvailableDart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("AppLogs","Request for Dart quiz");
+                Log.d("AppLogs", "Request for Dart quiz");
                 Toast.makeText(MainActivity.this, "Quiz not available", Toast.LENGTH_SHORT).show();
                 // intent_QuizPage.putExtra(EXTRA_QuizPage,"Dart");
                 // startActivity(intent_QuizPage);
             }
         });
 
-
-
     }
-
 
 }
--- AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
+++ AndriodStudioQuiz/app/src/main/java/summerproject/corona/andriodstudioquiz/QuizPage.java
@@ -1,4 +1,5 @@
 package summerproject.corona.andriodstudioquiz;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -26,21 +27,20 @@ public class QuizPage extends AppCompatActivity {
     private String[] Questions;
     private boolean[] Answers;
     private int score;
-    private int index=0;
-    private String scoreText="0/0";
-    public static  String EXTRA_QuizScore="summerproject.corona.codingquiz.key.quizscore"; // Unique
+    private int index = 0;
+    private String scoreText = "0/0";
+    public static String EXTRA_QuizScore = "summerproject.corona.codingquiz.key.quizscore"; // Unique
 
-//    public static void visitScorecard(){
-//        return;
-//    }
+    // public static void visitScorecard(){
+    // return;
+    // }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_page);
 
-        Log.d("AppLogs","Entered in Quiz Page");
-
+        Log.d("AppLogs", "Entered in Quiz Page");
 
         Intent intent = getIntent();
         String subject = intent.getStringExtra(MainActivity.EXTRA_QuizPage);
@@ -49,43 +49,38 @@ public class QuizPage extends AppCompatActivity {
         title.setText(subject);
         score = 0;
         // Questions for JAVA :
-        if(subject.toLowerCase().equals("java")){
-            Questions = new String[]
-                    {
+        if (subject.toLowerCase().equals("java")) {
+            Questions = new String[] {
                     "In an instance method or a constructor, \"this\" is a reference to the current object.",
                     "Garbage Collection is manual process.\n",
                     "The JRE deletes objects when it determines that they are no longer being used. This process is called Garbage Collection",
                     "Assignment operator is evaluated Left to Right",
                     "All binary operators except for the assignment operators are evaluated from Left to Right",
-                    "Java programming is not statically-typed, means all variables should not first be declared before they can be used."
-            };
+                    "Java programming is not statically-typed, means all variables should not first be declared before they can be used." };
 
-            Answers = new boolean[]{true,false,true,false,true,false};
+            Answers = new boolean[] { true, false, true, false, true, false };
 
-        }else if(subject.toLowerCase().equals("python")){
-            Questions = new String[]{
-                    "The condition x <= y <= z is allowed in Python",
-                    "Python does not have a Switch or Case statement",
-                    "Python strings are indeed mutable",
+        } else if (subject.toLowerCase().equals("python")) {
+            Questions = new String[] { "The condition x <= y <= z is allowed in Python",
+                    "Python does not have a Switch or Case statement", "Python strings are indeed mutable",
                     "In call-by-value, the argument whether an expression or a value gets bound to the respective variable in the function.",
                     "Built-in functions in Python - id() accepts one parameter and returns a unique identifier associated with the input object.",
                     "pass and continue are same in Python",
-                    "'chr(int)' returns the string denoting a character whose Unicode code point is an integer, Whereas 'ord(char)' in Python takes a string of size one and returns an integer denoting the Unicode code."};
+                    "'chr(int)' returns the string denoting a character whose Unicode code point is an integer, Whereas 'ord(char)' in Python takes a string of size one and returns an integer denoting the Unicode code." };
 
-            Answers = new boolean[]{true,true,false,true,true,false,true};
+            Answers = new boolean[] { true, true, false, true, true, false, true };
         }
 
-
-        final Intent intent_QuizScore = new Intent(this,QuizScore.class);
+        final Intent intent_QuizScore = new Intent(this, QuizScore.class);
 
         // ScoreCard Button
         scorecard = (Button) findViewById(R.id.scorecard);
         scorecard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scoreText = score+"/"+Questions.length;
-                Log.d("AppLogs","Request for Score");
-                intent_QuizScore.putExtra(EXTRA_QuizScore,scoreText);
+                scoreText = score + "/" + Questions.length;
+                Log.d("AppLogs", "Request for Score");
+                intent_QuizScore.putExtra(EXTRA_QuizScore, scoreText);
                 startActivity(intent_QuizScore);
             }
         });
@@ -96,56 +91,50 @@ public class QuizPage extends AppCompatActivity {
         buttonTrue = findViewById(R.id.buttonTrue);
         buttonFalse = findViewById(R.id.buttonFalse);
 
-
         buttonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizPage.this, "Answer Recorded : True", Toast.LENGTH_SHORT).show();
-                if(Answers[index]){
+                if (Answers[index]) {
                     score++;
 
                 }
-                if(++index==Questions.length){
-                    Log.d("AppLogs","Quiz Over! redirect to scorecard");
+                if (++index == Questions.length) {
+                    Log.d("AppLogs", "Quiz Over! redirect to scorecard");
                     scorecard.performClick();
-                }else{
+                } else {
                     question.setText(Questions[index]);
                 }
 
             }
         });
 
-
         buttonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizPage.this, "Answer Recorded : False", Toast.LENGTH_SHORT).show();
-                if(!Answers[index]){
+                if (!Answers[index]) {
                     score++;
 
                 }
-                if(++index==Questions.length){
-                    Log.d("AppLogs","Quiz Over! redirect to scorecard");
+                if (++index == Questions.length) {
+                    Log.d("AppLogs", "Quiz Over! redirect to scorecard");
                     scorecard.performClick();
-                }else{
+                } else {
                     question.setText(Questions[index]);
                 }
             }
         });
 
         // CountDown
-        countdown = (TextView) findViewById( R.id.countdown );
-        new CountDownTimer(30000*(Questions.length), 1000)  // milli seconds
+        countdown = (TextView) findViewById(R.id.countdown);
+        new CountDownTimer(30000 * (Questions.length), 1000) // milli seconds
         {
-            @SuppressLint({"DefaultLocale", "SetTextI18n"})
+            @SuppressLint({ "DefaultLocale", "SetTextI18n" })
             public void onTick(long millisUntilFinished) {
-                countdown.setText(
-                        String.format("%d min, %d sec",
-                                TimeUnit.MILLISECONDS.toMinutes( millisUntilFinished),
-                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)
-                                        - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
-                        )
-                );
+                countdown.setText(String.format("%d min, %d sec", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)
+                                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
             }
 
             @SuppressLint("SetTextI18n")
